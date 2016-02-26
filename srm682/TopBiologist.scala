f9ba13dcdf20008b400c35113bd47872247b1ddf
brandondsherman <brandondsherman@gmail.com>
2016-02-26 12:10:05 -0800

    TopBiologist was empty before

--- srm682/TopBiologist.scala
+++ srm682/TopBiologist.scala
@@ -1,24 +1,28 @@
-object test {	
-	def findShortestNewSequence(sequence: String): Option[String] ={
+object test {
+	def findShortestNewSequence(sequence: String): String = {
 		var found: Option[String] = None
-		for(depth <- 0 to sequence.length+1){
-			found = DLS("",depth, sequence)
-			if(found.isDefined) return found
+		var i = 0
+		for(i <- 0 to sequence.length){
+			found = dls("", i, sequence)
+			if(found.isDefined) return found.get
 		}
-		return None
-
+		return ""
 	}
 
-	def DLS(node : String, depth : Int, sequence : String): Option[String] ={
+	def dls(node: String, depth: Int, sequence: String): Option[String] = {
 		var found: Option[String] = None
 		if(depth == 0 && !sequence.contains(node) && node != ""){
 			return Some(node)
 		}else if(depth > 0){
-			for(c <- "ACTG"){
-				found = DLS(node + c,depth-1,sequence)
-				if(found.isDefined)return found
-			}	
-		}
+			for(c <- "ACGT"){
+				found = dls(node + c,depth-1,sequence)
+				if(found.isDefined) return found
+			}
+		} 
 		return None
 	}
 }
+
+
+
+
